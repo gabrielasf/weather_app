5855833b2c2e12cc2f6e7b809dcb05397652bd4b
gabrielasf <gabfloriani@gmail.com>
2020-06-15 01:46:34 +0200

    some improv after first fetch

--- src/App.js
+++ src/App.js
@@ -4,44 +4,64 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      loading: false,
+      location: "",
       municipios: [],
-      isLoaded: false,
+      error: false
     };
   }
 
-  componentDidMount() {
-    fetch('https://www.el-tiempo.net/api/json/v2/provincias/08/municipios')
-      .then(res => res.json())
-      .then(result => {
-        this.setState({
-          isLoaded: true,
-          municipios: result.municipios
-        });
-        console.log(result);
-      });
+  handleInput = event => {
+    this.setState({
+      location: event.target.value
+    });
   }
 
+  getWeather = () => {
+   this.setState({
+     loading: true,
+     weather: null,
+     error: false
+    });
+
+     fetch("https://www.el-tiempo.net/api/json/v2/provincias/08/municipios")
+      .then((res) => res.json())
+      .then(data => {
+       
+          this.setState({
+            municipios: data.municipios,
+            loading: false
+          });
+       console.log(data.municipios)
+      });
+  };
+
+
   render() {
-    const { municipios, isLoaded } = this.state;
-    if (!isLoaded) {
-      return <div>Loading ... </div>;
-    } else {
-      return (
-        <ul>
+    return (
+      <div>
+        <input
+          type="text"
+          name="location"
+          value={this.state.location}
+          onChange={this.handleInput}
+        />
+        
+        {this.state.loading && <div>Loading...</div>}
+        {this.state.error && <div>{this.state.error}</div>}
+        
           {municipios.map(municipio => (
-            <li key={municipio.id_rel}>  
-              <h3>{municipio.NOMBRE} </h3>
-              
+            <li key={municipio.ID_REL}>  
+              <h3>{municipio.NOMBRE}</h3>
+              <h4>{municipio.COD_GEO}</h4>
+          <h4>{this.state.location}</h4>
             </li>
           ))}
-        </ul>
-      );
-    }
+        
+      </div>
+    );
   }
 }
 
 export default App;
-
-
-
 
---
+++ src/components/MunicipiosList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MunicipiosList = () => {
+    return (
+        <div className ="muni-list">
+            <ul>
+                <li>Abrera</li>
+                <li>Platja d'Aro</li>
+            </ul>
+
+        </div>
+    );
+}
+
+export default MunicipiosList;
